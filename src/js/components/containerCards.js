b9b9c21551e7c87c0fc1035dc899387ea40c5a1c
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-11-12 10:21:07 -0300

    Ajustes de contenedores de cartas productos en view de busqueda y carrito

--- src/js/components/containerCards.js
+++ src/js/components/containerCards.js
@@ -1,7 +1,10 @@
 import { productCard } from "./productCard.js";
+import { searchForm } from "./searchForm.js";
 export const containerCards = (data) => {
     const containerCards = document.createElement('div');
     containerCards.classList.add('row', 'justify-content-center');
+    const form = searchForm();
+    containerCards.appendChild(form);
     data.forEach(item=>{
         let col = document.createElement('div');
         col.classList.add('col-auto', 'my-1');
--- src/js/components/containerCart.js
+++ src/js/components/containerCart.js
@@ -1,19 +1,44 @@
+import { numToContable } from "../utils/utils.js";
 import { cardCart } from "./cartCard.js";
 
 export const containerCart = ()=>{
     var cartStorega = JSON.parse(localStorage.getItem('cart'));
     const response = document.createElement('div');
+    response.classList.add('row');
     if (cartStorega && cartStorega.length>0){
+        const containerCards = document.createElement('div');
+        containerCards.classList.add('col');
         cartStorega.forEach(item =>{
             let card = cardCart(item);
-            response.appendChild(card);
+            containerCards.appendChild(card);
         });
+        const bill = document.createElement('div');
+        bill.classList.add('col-auto');
+        bill.innerHTML = `
+            <h3>Detalle Compra</h3>
+            <ul class="list-group"></ul>
+            <hr>
+            <h4></h4>
+        `;
+        var total = 0;
+        cartStorega.forEach(({product,amount}=item)=>{
+            let li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.innerHTML = `
+                ${product.name} x ${amount} = ${numToContable(amount*product.price)}
+            `;
+            bill.querySelector('ul').appendChild(li);
+            total+=amount*product.price;
+        });
+        bill.querySelector('h4').innerText = `Total = ${numToContable(total)}`;
+        response.appendChild(containerCards);
+        response.appendChild(bill);
     } else {
         response.innerHTML = `
         <h1 class="text-center d-flex m-auto align-self-center">
-        No tienes productos en el carro :( 
+        No tienes productos. Busca tus favoritos ☺
         </h1>
-        <p class="text-center">Agregalos aca</p>
+
         `;
     }
     return response;
