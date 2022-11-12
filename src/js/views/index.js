dd72f9c5f7f62efa74e2c3d07bb33e40fe018d8d
Bryan Alburquenque <alburquenque.letelier@gmail.com>
2022-11-12 10:20:05 -0300

    Se ajusta index para desplegar componentes

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,8 +1,9 @@
 import {apiUrl} from "../../config.js";
-import {get_categories, get_products} from "../api/apiGET.js";
+import {get_products} from "../api/apiGET.js";
 import {containerCards} from "../components/containerCards.js";
 import {leftNavBar} from "../components/leftNavBar.js";
 import { loading } from "../utils/utils.js";
+import { searchForm } from "../components/searchForm.js";
 
 export const indexView = async (state)=>{
     // <div id="main" class="d-sm-flex flex-sm-row flex-column">
@@ -10,16 +11,16 @@ export const indexView = async (state)=>{
     //         </div>
     //         <div id="productsContainer" class="p-1 bg-light"></div>
     //     </div>
-    if (!document.querySelector('#main')){
-        const main = document.createElement('div');
-        main.classList.add('d-sm-flex', 'flex-sm-row', 'flex-column');
-        main.innerHTML = `
-            <div id="leftNav" class="border-end border-1"></div>
-            <div id="productsContainer" class="p-1 bg-light"></div>
-        `;
-        document.querySelector('.container-fluid').insertBefore(main, document.querySelector('footer'));
-    };
-    
+    // if (!document.querySelector('#main')){
+    //     const main = document.createElement('div');
+    //     main.classList.add('d-sm-flex', 'flex-sm-row', 'flex-column');
+    //     main.innerHTML = `
+    //         <div id="leftNav" class="border-end border-1"></div>
+    //         <div id="productsContainer" class="p-1 bg-light"></div>
+    //     `;
+    //     document.querySelector('.container-fluid').insertBefore(main, document.querySelector('footer'));
+    // };
+    const form = searchForm();
     var products = "";
     if (state){
         products = state;
@@ -31,5 +32,4 @@ export const indexView = async (state)=>{
     };
     loading('leftNav', leftNavBar);
     loading('productsContainer',containerCards, products);
-    history.pushState({page: 'index', load: {tag: state ? state.load.tag : '', functionCall: state ? state.load.functionCall : '', args: state ? state.load.args : null}}, '');
 };
